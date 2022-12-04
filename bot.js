9f0e6bcc9555fc224d00d275b613d7ac18657ff5
Eoin Halligan <eoin@losant.com>
2022-12-03 21:39:36 -0500

    baker sucks

--- bot.js
+++ bot.js
@@ -1,43 +1,18 @@
 import { Telegraf } from "telegraf";
-import { getRandomId } from "./pictures.js";
-import imageSearch from 'image-search-google';
-import { telegrafKey, gcsId, gcsApiKey } from "./config.js";
-import { promptBuilder } from './prompt-builder'
+import { telegrafKey } from "./config.js";
+import { buildPrompt } from './prompt-builder.js'
 const bot = new Telegraf(telegrafKey);
-
 
 bot.on('text', async (ctx) => {
   // Explicit usage
-  if (ctx.message.text.includes('hey baker')) {
+  if (ctx.message.text?.toLocaleLowerCase().includes('hey baker')) {
     const index = ctx.message.text.indexOf('hey baker')
     const question = ctx.message.text.slice(index + 9).trim()
-    const prompt = `respond as baker mayfield to this: ${question}`
-    const result = await promptBuilder(prompt, 0.5);
-    //make OPEN API CALL HERE WITH THE PROMP VARIBALE
+    const prompt = `respond as Baker Mayfield to this: ${question}`
+    const result = await buildPrompt(prompt, 0.9);
+    console.log(result)
     await ctx.telegram.sendMessage(ctx.message.chat.id, result)
   }
 });
-
-bot.hears(regex, async (ctx) => {
-  gcsClient.search('Baker Mayfield is trash', { page: 1, size: 'large' }).then(async images => {
-    const index = Math.floor(Math.random() * (images.length + 1));
-    const url = images[index]?.url;
-    if (url) {
-      try {
-        bot.telegram.sendPhoto(ctx.chat.id, { url })
-      } catch (e) {
-        console.error(e);
-      }
-
-    } else {
-      const pictureId = await getRandomId()
-      try {
-        bot.telegram.sendPhoto(ctx.chat.id, { source: `./ res / ${pictureId}.jpg` })
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }).catch(err => console.error(err));
-})
 
 bot.launch();
--- prompt-builder.js
+++ prompt-builder.js
@@ -1,10 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import { openAiKey } from './config.js'
-import readline from 'readline';
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+
 const configuration = new Configuration({
   apiKey: openAiKey,
 });
@@ -15,5 +11,6 @@ export async function buildPrompt(prompt, temperature) {
         model: 'text-davinci-002', prompt, temperature, max_tokens: 50
     }
     const completion = await openai.createCompletion(config);
-    return completion.data.choices[0].text;
+    const response = completion.data.choices[0].text;
+    return response;
 }
