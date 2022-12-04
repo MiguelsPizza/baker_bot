4ee406480b499f0d301bb88191e0cf5d415f67d8
MiguelsPizza <alexmnahas@gmail.com>
2022-12-03 22:09:22 -0500

    updated to new chatbot model

--- prompt-builder.js
+++ prompt-builder.js
@@ -7,7 +7,7 @@ const openai = new OpenAIApi(configuration);
 
 export async function buildPrompt(prompt, temperature) {
   const config = {
-    model: 'text-davinci-002', prompt, temperature, max_tokens: 50
+    model: 'text-davinci-003', prompt, temperature, max_tokens: 50
   }
   const completion = await openai.createCompletion(config);
   const response = completion.data.choices[0].text;
