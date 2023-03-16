9efd7490f8d5a3827c55c6396339ab3c0e780986
MiguelsPizza <alexmnahas@gmail.com>
2023-03-15 21:27:16 -0400

    This sorta works. need to find the right vec similarity percentage. baker interjects a bit too much rn

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
 import { Telegraf } from "telegraf";
-import { buildPrompt } from './prompt-builder.js'
-const bot = new Telegraf(
-  process?.env?.telegrafKey ?? "5573440835:AAE8vQwwLQQw6ovO00Yskz_V00F5MluStb8"
-)
+import { buildPrompt, vetInput } from "./prompt-builder.js"
+import config from "./config.js"
+const bot = new Telegraf(process?.env?.telegrafKey ?? config.telegrafKey)
 
 const queue = []
 
 bot.on("text", async (ctx) => {
   // Explicit usage
   queue.push({ role: "user", content: ctx.message.text })
-
-  if (ctx.message.text?.toLocaleLowerCase().includes("hey baker")) {
+  //old test ctx.message.text?.toLocaleLowerCase().includes("hey baker")
+  if (vetInput(ctx.message.text?.toLocaleLowerCase())) {
     const result = await buildPrompt(queue)
     await ctx.telegram.sendMessage(ctx.message.chat.id, result.content)
     queue.push(result)
