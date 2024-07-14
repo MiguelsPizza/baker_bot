045d154d1f0441a44acf81472b1e8ccd0f5a4bc1
Eoin Halligan <eoin@losant.com>
2024-07-13 23:45:42 -0400

    added ui

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export default {
-  telegrafKey: "5573440835:AAEYh32l6HHWjTaAgZ3uIgwN0Ha6XQH3B9c",
-  gcsId: "f54fc27c6816542ce",
-  gcsApiKey: "AIzaSyDKOtLnF4CeX00k4xrmqKTW5VsPpgRzFRQ",
-  openaiToken: "sk-proj-mVNBdfZCTzMFRXJceZpVT3BlbkFJbswPh33Lap2AODrRhZZB"
+  telegrafKey: "",
+  gcsId: "",
+  gcsApiKey: "",
+  openaiToken: ""
 };
